77f1089563d4a596fed7fef2cf876d2cae3f9582
Guillom8769 <8769@holbertonstudents.com>
2024-06-11 20:52:57 +0200

    test flask restx

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,19 @@
 from flask import Flask, jsonify
+from flask_restx import Api
+from .api.v1.endpoints import users, places, reviews, amenities, cities, countries
 
 def create_app():
     app = Flask(__name__)
+    api = Api(app, version='1.0', title='HBnB API',
+              description='A simple API for HBnB Evolution project')
 
-    from .api.v1.endpoints import users, places, reviews, amenities, cities, countries
-    app.register_blueprint(users.bp)
-    app.register_blueprint(places.bp)
-    app.register_blueprint(reviews.bp)
-    app.register_blueprint(amenities.bp)
-    app.register_blueprint(cities.bp)
-    app.register_blueprint(countries.bp)
+    api.add_namespace(users.bp, path='/api/v1/users')
+    api.add_namespace(places.bp, path='/api/v1/places')
+    api.add_namespace(reviews.bp, path='/api/v1/reviews')
+    api.add_namespace(amenities.bp, path='/api/v1/amenities')
+    api.add_namespace(cities.bp, path='/api/v1/cities')
+    api.add_namespace(countries.bp, path='/api/v1/countries')
 
-    # Root route
     @app.route('/')
     def home():
         return jsonify(message="Welcome to the HBnB API")
@@ -19,7 +21,3 @@ def create_app():
     return app
 
 app = create_app()
-
-if __name__ == "__main__":
-    print("Starting the HBnB API server...")
-    app.run(host="0.0.0.0", port=5000)
--- app/api/v1/endpoints/users.py
+++ app/api/v1/endpoints/users.py
@@ -1,8 +1,21 @@
 from flask import Blueprint, request, jsonify
+from flask_restx import Namespace, Resource, fields
 from app.models.user import User
 
+# Définition du Blueprint
 bp = Blueprint('users', __name__, url_prefix='/users')
 
+# Définition du Namespace et du modèle Flask-RESTX
+user_ns = Namespace('users', description='User operations')
+
+user_model = user_ns.model('User', {
+    'id': fields.String(required=True, description='The user identifier'),
+    'email': fields.String(required=True, description='The user email'),
+    'first_name': fields.String(required=True, description='The user first name'),
+    'last_name': fields.String(required=True, description='The user last name'),
+})
+
+# Routes du Blueprint
 @bp.route('/', methods=['POST'])
 def create_user():
     data = request.get_json()
@@ -42,3 +55,59 @@ def delete_user(user_id):
         return jsonify({'error': 'User not found'}), 404
     user.delete()
     return '', 204
+
+# Ressources Flask-RESTX
+@user_ns.route('/')
+class UserList(Resource):
+    @user_ns.doc('list_users')
+    @user_ns.marshal_list_with(user_model)
+    def get(self):
+        '''List all users'''
+        users = User.get_all()
+        return users
+
+    @user_ns.doc('create_user')
+    @user_ns.expect(user_model)
+    @user_ns.marshal_with(user_model, code=201)
+    def post(self):
+        '''Create a new user'''
+        data = request.get_json()
+        user = User(email=data['email'], password=data['password'], first_name=data['first_name'], last_name=data['last_name'])
+        user.save()
+        return user, 201
+
+@user_ns.route('/<string:user_id>')
+class UserResource(Resource):
+    @user_ns.doc('get_user')
+    @user_ns.marshal_with(user_model)
+    def get(self, user_id):
+        '''Fetch a user given its identifier'''
+        user = User.get(user_id)
+        if not user:
+            user_ns.abort(404, "User not found")
+        return user
+
+    @user_ns.doc('update_user')
+    @user_ns.expect(user_model)
+    @user_ns.marshal_with(user_model)
+    def put(self, user_id):
+        '''Update a user given its identifier'''
+        user = User.get(user_id)
+        if not user:
+            user_ns.abort(404, "User not found")
+        data = request.get_json()
+        user.email = data.get('email', user.email)
+        user.password = data.get('password', user.password)
+        user.first_name = data.get('first_name', user.first_name)
+        user.last_name = data.get('last_name', user.last_name)
+        user.save()
+        return user
+
+    @user_ns.doc('delete_user')
+    def delete(self, user_id):
+        '''Delete a user given its identifier'''
+        user = User.get(user_id)
+        if not user:
+            user_ns.abort(404, "User not found")
+        user.delete()
+        return '', 204
